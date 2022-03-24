124daed668fe297c50da154906a1b1ff8dcf27c1
Mistakx <ade9@live.com.pt>
2022-03-24 13:44:53 +0000

    Parse requests function now properly continues after finding a blank line
    Signed-off-by: Mistakx <ade9@live.com.pt>

--- pa-web-server-master/src/test/MainTest.java
+++ pa-web-server-master/src/test/MainTest.java
@@ -29,18 +29,13 @@ class MainTest {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
         }).start();
 
     }
 
     @DisplayName("Server accepting connections")
     @Test
-    void testMain() throws IOException, InterruptedException {
+    void serverAcceptingConnections() throws IOException, InterruptedException {
         Properties serverConfig = new Properties();
         InputStream configPathInputStream = new FileInputStream(serverConfigPath);
         serverConfig.load(configPathInputStream);
@@ -49,6 +44,44 @@ class MainTest {
         // https://stackoverflow.com/questions/3489543/how-to-call-a-method-with-a-separate-thread-in-java
         final boolean[] serverExists = {false};
         Thread testServerConnectionThread = new Thread(() -> {
+
+            // Sleep 2 seconds to wait for server start
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // Try to connect to server
+            try (Socket serverSocket = new Socket("127.0.0.1", serverPort)) {
+                serverExists[0] = true;
+                DataOutputStream serverDataOutputStream = new DataOutputStream(serverSocket.getOutputStream());
+                // https://www.javatpoint.com/socket-programming
+                serverDataOutputStream.writeUTF("");
+                serverDataOutputStream.flush();
+                serverDataOutputStream.close();
+            } catch (IOException ignored) {
+            }
+
+        });
+        testServerConnectionThread.start();
+        testServerConnectionThread.join();
+        assertTrue(serverExists[0]);
+
+    }
+
+    @DisplayName("Server returning page")
+    @Test
+    void serverSendingPage() throws IOException, InterruptedException {
+        Properties serverConfig = new Properties();
+        InputStream configPathInputStream = new FileInputStream(serverConfigPath);
+        serverConfig.load(configPathInputStream);
+        int serverPort = parseInt(serverConfig.getProperty("server.port"), 10);
+
+        // https://stackoverflow.com/questions/3489543/how-to-call-a-method-with-a-separate-thread-in-java
+        final boolean[] serverExists = {false};
+        Thread testServerConnectionThread = new Thread(() -> {
+
             try (Socket serverSocket = new Socket("127.0.0.1", serverPort)) {
                 serverExists[0] = true;
                 DataOutputStream serverDataOutputStream = new DataOutputStream(serverSocket.getOutputStream());
@@ -62,6 +95,6 @@ class MainTest {
         testServerConnectionThread.start();
         testServerConnectionThread.join();
         assertTrue(serverExists[0]);
-
     }
+
 }
